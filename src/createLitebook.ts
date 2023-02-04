ca7901f4c4b5fc6bb95c8a05ec06f0de7644aa20
mnik01 <maksimgrinberg01@gmail.com>
2023-02-04 15:05:06 +0600

    fix

--- src/createLitebook.ts
+++ src/createLitebook.ts
@@ -21,7 +21,7 @@ export const createLitebook = () => {
     templates["vite.config.ts"](storiesInUsersProject)
   );
 
-  console.log(`storiesInUsersProject: ${storiesInUsersProject}`);
+  stdout.write(`storiesInUsersProject: ${storiesInUsersProject}`);
   // Stories
   fs.mkdirSync(".litebook/stories");
   storiesInUsersProject.forEach(async (st) => {
