8e30ed6847178b835c7f0fb0487941eece4eeccb
mnik01 <maksimgrinberg01@gmail.com>
2023-02-04 14:16:14 +0600

    fix

--- src/createLitebook.ts
+++ src/createLitebook.ts
@@ -1,5 +1,5 @@
 import fs from "fs-extra";
-import { renderFile } from "template-file";
+import * as TF from "template-file";
 import glob from "glob";
 
 export const createLitebook = async () => {
@@ -12,23 +12,23 @@ export const createLitebook = async () => {
   fs.mkdirSync(".litebook");
   fs.writeFileSync(
     ".litebook/index.html",
-    await renderFile("./templates/index.html", {})
+    await TF.renderFile("./templates/index.html", {})
   );
   fs.writeFileSync(
     ".litebook/index.tsx",
-    await renderFile("./templates/index.tsx", {})
+    await TF.renderFile("./templates/index.tsx", {})
   );
   fs.writeFileSync(
     ".litebook/tsconfig.json",
-    await renderFile("./templates/tsconfig.json", {})
+    await TF.renderFile("./templates/tsconfig.json", {})
   );
   fs.writeFileSync(
     ".litebook/public/logo.svg",
-    await renderFile("./templates/public/logo.svg", {})
+    await TF.renderFile("./templates/public/logo.svg", {})
   );
   fs.writeFileSync(
     ".litebook/vite.config.ts",
-    await renderFile("./templates/vite.config.ts", {
+    await TF.renderFile("./templates/vite.config.ts", {
       stories: storiesInUsersProject.map((st) => ({
         lower: st.toLowerCase(),
         capitalize: st,
@@ -41,7 +41,7 @@ export const createLitebook = async () => {
   storiesInUsersProject.forEach(async (st) => {
     fs.writeFileSync(
       `.litebook/stories/${st}/iframe.tsx`,
-      await renderFile(`./templates/stories/Component/iframe.tsx`, {
+      await TF.renderFile(`./templates/stories/Component/iframe.tsx`, {
         name: st,
       })
     );
